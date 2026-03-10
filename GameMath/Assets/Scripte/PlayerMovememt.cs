19160d0ccaf06d8f8b789d6b90df4320ca54ba74
esuvean <>
2026-03-10 15:50:06 +0900

    Update : 플레이어 Sprint 수정중

---
+++ GameMath/Assets/Scripte/PlayerMovememt.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovememt : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    private Vector2 moveInput;
+
+    public void OnMove(InputValue value)
+    {
+        moveInput = value.Get<Vector2>();
+    }
+
+    void Update()
+    {
+        Vector3 direction = new Vector3(moveInput.x, moveInput,y, 0);
+        transform.TransLate(directiom*moveSpeed*Time.deltaTiom);
+    }
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+}
